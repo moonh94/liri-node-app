2f094e2f9fda5a33a472e15c6a9297bed5a691e5
Han Moon <hanmoon@Hans-MacBook-Pro.local>
2019-03-27 21:43:36 -0600

    final 2nd try

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # liri-node-app
+
+Image of spotify-this-song: 
+![image of spotify]
+(/assets/images/screenshot1.png)
+
+![image of bands]
+(/assets/images/screenshot2.png)
+
+![image of fs and movie]
+(/assets/images/screenshot3.png)
--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -2,41 +2,79 @@ require("dotenv").config();
 
 var keys = require("./key.js");
 var Spotify =require(`node-spotify-api`);
-var spotify = new Spotify(keys.Spotify);
-var song = process.argv[2];
-spotify.search({type: "track", query: song})
+var spotify = new Spotify({
+    id: "6360e6d7441b48babd652a0abd007790",
+    secret: "49dc057fad644a9caaf93e15339d5441"
+});
+
+var song = process.argv[3];
+if (process.argv[2]=== "spotify-this-song") {
+spotify.search({type:'track', query: song})
     .then(function(response) {
-        console.log(response.data);
+        for (var i = 0; i < response.tracks.items.length; i++) {
+        console.log(`Artist: ${response.tracks.items[i].artists[0].name} \nSong: ${response.tracks.items[i].name} \nAlbum: ${response.tracks.items[i].album.name} \nPreview link: ${response.tracks.items[i].preview_url} \n\n`);
+        }
     })
     .catch(function(err) {
         console.log(err);
     })
+    
+};
 
 
 var axios = require("axios");
-var artist = process.argv[2];
+var artist = process.argv[3];
 var bandsInTown = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+if(process.argv[2]=== "concert-this") {
 axios.get(bandsInTown)
     .then(function(response){
-        console.log(response.data.venue);
-        //can't get past object
-       // console.log(`Venue: ${response.data.venue.name} /nVenue location: ${response.data.venue.city}, ${response.data.venue.country} \nDate of Event: ${response.data.datetime}`);
-    });
+        for (var i=0; i<response.data.length; i++) {
+        console.log(`Venue: ${response.data[i].venue.name} \nCity: ${response.data[i].venue.city}, ${response.data[i].venue.country} \nDate: ${response.data[i].datetime} \n\n`);
+        }
+    })
+};
 
 
 
-var title = process.argv[2];
+var title = process.argv[3];
 var queryUrl= "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+if (process.argv[2]=== "movie-this") {
 axios.get(queryUrl)
     .then (function(response) {
-        console.log(`Title: ${response.data.Title} \nYear: ${response.data.Year} \nRating: ${response.data.imbdRating} \nRotten Tomatoes: ${response.data.Ratings[1]} \nCountry: ${response.data.Country} \nLanguage: ${response.data.Language} \nPlot: ${response.data.Plot} \nActors: ${response.data.Actors}`);
-    });
+        console.log(`Title: ${response.data.Title} \nYear: ${response.data.Year} \nRating: ${response.data.Rated} \nRotten Tomatoes: ${response.data.Ratings[1].Value} \nCountry: ${response.data.Country} \nLanguage: ${response.data.Language} \nPlot: ${response.data.Plot} \nActors: ${response.data.Actors} \n\n`);
+    })
+};
 
 var fs = require("fs");
+var terms = [];
+if (process.argv[2] === "do-what-it-says") {
 fs.readFile("random.txt","utf8", function(error, data) {
     if (error) {
         return console.log(error);
-    }else {
-    console.log(data);
-    }
-});
+    } else {
+    var dataArr = data.split(", ");
+dataArr.forEach(function(song) {
+    console.log(song);
+
+
+//     terms.push(dataArr);
+//     console.log(terms);
+//         spotify.search({type:'track', query: terms[1]})
+//     .then(function(response) {
+//         for (var i = 0; i < response.tracks.items.length; i++) {
+//         console.log(`Artist: ${response.tracks.items[i].artists[0].name} \nSong: ${response.tracks.items[i].name} \nAlbum: ${response.tracks.items[i].album.name} \nPreview link: ${response.tracks.items[i].preview_url} \n\n`);
+//         }
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     })
+// }
+// dataArr.forEach(function(song) {
+//     console.log(song);
+    
+
+//     }
+})
+}
+})
+}
